39e3982e3b0cfe534ade19cdafdb8f4116ac1c25
espaya <beehivelive8@gmail.com>
2025-02-01 14:23:59 +0000

    Added documents link

--- resources/views/customer/docs/index.blade.php
+++ resources/views/customer/docs/index.blade.php
@@ -10,9 +10,29 @@
             <div class="container">
                 <div class="content-main lg:px-[60px] md:px-4 flex gap-y-8 max-md:flex-col w-full">
                     @include('customer/menu/menu')
-                    <div class="right xl:w-2/3 md:w-7/12 w-full xl:pl-[40px] lg:pl-[28px] md:pl-[16px] flex items-center">
-                        <!-- Add information here -->
+                    <div class="right xl:w-2/3 md:w-7/12 w-full xl:pl-[40px] lg:pl-[28px] md:pl-[16px] flex items-start justify-center">
+                        <div class="w-full">
+                            <ul class="pl-5 text-left space-y-2">
+                                <li>☛ <a href="#" class="text-blue-600 hover:underline">Advanced Directive Acknowledgment - HIPAA Home Care Privacy Rights</a></li>
+                                <li>☛ <a href="#" class="text-blue-600 hover:underline">Authorization Agreement And Acknowledgment</a></li>
+                                <li>☛ <a href="#" class="text-blue-600 hover:underline">Authorization For Use And Disclosure of Protected Health Information</a></li>
+                                <li>☛ <a href="#" class="text-blue-600 hover:underline">Charges For Services</a></li>
+                                <li>☛ <a href="#" class="text-blue-600 hover:underline">Confidentiality Policy</a></li>
+                                <li>☛ <a href="#" class="text-blue-600 hover:underline">Consent For Services And Financial Agreement</a></li>
+                                <li>☛ <a href="#" class="text-blue-600 hover:underline">Consumer Bill of Rights</a></li>
+                                <li>☛ <a href="#" class="text-blue-600 hover:underline">Consumer Emergency And Contact Information</a></li>
+                                <li>☛ <a href="#" class="text-blue-600 hover:underline">Contract Form Amended 2024</a></li>
+                                <li>☛ <a href="#" class="text-blue-600 hover:underline">Contractor Bio Review</a></li>
+                                <li>☛ <a href="#" class="text-blue-600 hover:underline">Discrimination Bye Laws</a></li>
+                                <li>☛ <a href="#" class="text-blue-600 hover:underline">HIPAA</a></li>
+                                <li>☛ <a href="#" class="text-blue-600 hover:underline">List of Services Provided</a></li>
+                                <li>☛ <a href="#" class="text-blue-600 hover:underline">Policy For Investigating Any Grievances By a Client or Designated Representative</a></li>
+                                <li>☛ <a href="#" class="text-blue-600 hover:underline">Reporting Patient Abuse And Neglect</a></li>
+                            </ul>
+                        </div>
                     </div>
+
+
                 </div>
             </div>
         </div>
