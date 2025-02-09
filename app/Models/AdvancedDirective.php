44e74a288f8d2d6ea671bc54f8819f5d3a4d30ef
espaya <beehivelive8@gmail.com>
2025-02-09 01:13:52 +0000

    Fixed table name

--- app/Models/AdvancedDirective.php
+++ app/Models/AdvancedDirective.php
@@ -9,4 +9,18 @@ class AdvancedDirective extends Model
 {
     use HasFactory;
 
-    protected $fillable = [];}
+    protected $table = 'advanced_directive';
+
+    protected $fillable = [
+        'medical_record_number',
+        'living_will',
+        'statutory_power',
+        'confirm',
+        'clients_signature',
+        'clients_signed_date',
+        'agency_witness_name',
+        'agency_witness_signature',
+        'agency_signed_date',
+        'customerID',
+    ];
+}
